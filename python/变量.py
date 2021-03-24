a23bd54ae0db9885853379ee0aab23afa3fa49e7
戏子 <2568942844@qq.com>
2021-03-24 20:00:22 +0800

    删除文件 python

--- python/变量.py
+++
@@ -1,3 +0,0 @@
-print(int(-12.69), int('100'), int('100', 2), int('100', 16), int(True))
-print(float(-2), float('10'), float(True))
-
--- python/逻辑表达式.py
+++
@@ -1,3 +0,0 @@
-print(6<9,not 3>5,not 6>9,not 6,not '6',not 0.0,not True)#判断大于小于是否正确
-print(3>5 and 6<9,not 3>5 and 6<9,3>5 and not 6>9)#判断大于小于是否正确
-print(3>5 or 6<9,not 3>5 or 6>9,3>5 or not 6<9)#判断大于小于是否正确
