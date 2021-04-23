bcc9f60b7e82762b8edbd0559659828f4679b1e6
戏子 <2568942844@qq.com>
2021-04-23 08:07:47 +0800

    读取和陷入

--- 爬虫/给爷爬股吧.py
+++ 爬虫/给爷爬股吧.py
@@ -1,5 +1,12 @@
+import csv
+
+import pandas as pd
 import requests
 from lxml import etree
+import pandas as pd
+from openpyxl import Workbook
+import xlwt
+import xlrd
 
 url = 'https://guba.eastmoney.com/'
 
@@ -10,6 +17,7 @@ text = requests.get(url, headers=header)  # 头
 text.encoding = "utf-8"  # 转gbk
 html = etree.HTML(text.text)  # 解析
 result = html.xpath('//ul[@class="newlist"]/li')
+all_products = []
 for i in result:
     comp = i.xpath('./cite/text()')
     readinfo = comp[0].strip()  # 阅读
@@ -18,6 +26,24 @@ for i in result:
     Authorinfo = i.xpath('./cite[@class="aut"]/a/font/text()')  # 作者
     # Authorinfo=Authorinfo.xpath('./a/font/text()')
     Update = comp[5].strip()  # 时间
+    all_products.append([  # 创建excel表
+        readinfo,
+        commentinfo,
+        titleinfo,
+        Authorinfo,
+        Update,
+    ])
+    # print(all_products)
+    keys = all_products[0]
 
-    print('阅读', readinfo, '评论', commentinfo, '标题', titleinfo, '作者', Authorinfo, '时间', Update)
-# print(result)
+    book = xlwt.Workbook()  # 创建一个workbook对象
+    sh = book.add_sheet("股吧")
+    col_name = ["阅读", "评论", "标题", "作者", "时间"]
+    for col, name in enumerate(col_name):
+        sh.write(0, col, name)
+
+    for i, line in enumerate(all_products):
+        for j, item in enumerate(line):
+            sh.write(i + 1, j, item)
+            pass
+    book.save(r"D:\Desktop\股吧.xls")
